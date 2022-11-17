e91ed6dd226d496d28913454c3b416266d0037f4
prach19 <prachi19@my.yorku.ca>
2022-11-16 22:22:38 -0500

    added about, button functions

---
+++ buttons.js
@@ -0,0 +1,9 @@
+function aboutButton(){
+    window.open("./about.html");
+}
+function logIn(){
+    window.open("./index.html");
+}
+function github(){
+    window.open("https://github.com/prach19/lock_n_code");
+}
